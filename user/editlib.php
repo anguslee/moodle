01106b2647699bfd80e7d7ec9f7c499fbcc38de7
LI Geng <angus.lee8329@gmail.com>
2014-03-02 17:17:22 +0800

    move unit function to its own module

--- user/editlib.php
+++ user/editlib.php
@@ -455,37 +455,3 @@ function useredit_get_disabled_name_fields($enabledadditionalusernames = null) {
             array_merge(array('firstname', 'lastname'), $enabledadditionalusernames));
     return $nonusednamefields;
 }
-
-function unit_edit_shared_definition(&$mform, $unit_id) {
-    global $CFG, $USER, $DB;
-
-    $unit = $DB->get_record('unit', array('id' => $unit_id));
-
-    $strrequired = get_string('required');
-
-    $mform->addElement('text', 'unit_id',  '班级编号',  'maxlength="100" size="30"');
-    $mform->addRule('unit_id', $strrequired, 'required', null, 'client');
-    $mform->setType('unit_id', PARAM_NOTAGS);
-
-    $mform->addElement('text', 'name',  '班级名称',  'maxlength="100" size="30"');
-    $mform->addRule('name', $strrequired, 'required', null, 'client');
-    $mform->setType('name', PARAM_NOTAGS);
-
-    $mform->addElement('text', 'entry_year', '入学年份', 'maxlength="20" size="25"');
-    $mform->addRule('entry_year', $strrequired, 'required', null, 'client');
-    $mform->setType('entry_year', PARAM_INT);
-
-    $mform->addElement('select', 'flag', '类别', array(htmlspecialchars('文') => '文科班',
-                                                       htmlspecialchars('理') => '理科班',
-                                                       htmlspecialchars('N/A') => 'N/A'));
-    $mform->addRule('flag', $strrequired, 'required', null, 'client');
-    $mform->setDefault('flag', $unit->flag);
-    
-    $mform->addElement('text', 'initial_entry_year', '起始年度', 'maxlength="20" size="25"');
-    $mform->addRule('initial_entry_year', $strrequired, 'required', null, 'client');
-    $mform->setType('initial_entry_year', PARAM_INT);
-
-    $mform->addElement('text', 'classroom_location', '教室位置', 'maxlength="255" size="25"');
-    $mform->setType('classroom_location', PARAM_TEXT);
-
-}
